5a3e42a5a793f928ba11368dcf84c50dda1bf1ca
Danilo Hernandez <dhernandez0032@gmail.com>
2021-06-07 15:21:49 -0600

    single page and front page

--- functions.php
+++ functions.php
@@ -44,6 +44,32 @@ function sidebar() {
 
 add_action('widgets_init', 'sidebar');
 
+function productos_type() {
+	$labels = array(
+		'name' => 'Productos',
+		'singular_name' => 'Producto',
+		'menu_name' => 'Productos'
+	);
+
+	$args = array(
+		'label' => 'Productos',
+		'description' => 'Productos de platzi',
+		'labels' => $labels,
+		'supports' => array('title', 'editor', 'thumbnail', 'revisions'),
+		'public' => true,
+		'show_in_menu' => true,
+		'menu_position' => 5,
+		'menu_icon' => 'dashicons-cart',
+		'can_export' => true,
+		'publicly_queryable' => true,
+		'rewrite' => true,
+		'show_in_rest' => true
+	);
+	register_post_type('producto', $args);
+}
+
+add_action('init', 'productos_type');
+
 
 
 
