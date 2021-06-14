ba3fca2b75dcc5698edd961363100e9aa6d8ed6b
Danilo Hernandez <dhernandez0032@gmail.com>
2021-06-14 14:37:39 -0600

    404

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header() ?>
+
+<main class='container'>
+    <div class="pagina404 my-5">
+        <h1 class="text-center">
+        	<span class="text-warning display-lg">404</span>
+        	PÁGINA NO ENCOTRADA
+        </h1>
+        <h2 class="text-center text-lead text-white">Haga <a href="<?php echo home_url(); ?>">click aquí</a>  para volver a la página principal</h2>
+    </div>
+</main>
+
+<?php get_footer() ?>
